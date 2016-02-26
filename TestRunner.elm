74bb2a780091dd8e42a923d7180fcad1ac0c6095
Vyacheslav Voronchuk <voronchuk@gmail.com>
2016-02-26 19:01:16 +0200

    Test runner added

---
+++ TestRunner.elm
@@ -0,0 +1,26 @@
+module Main where
+
+
+import ElmTest exposing (..)
+import Console exposing (..)
+import Task
+import List
+
+import Hexagons.Tests
+
+
+allTests : List Test
+allTests = 
+    [
+        Hexagons.Tests.hex,
+        Hexagons.Tests.layout,
+        Hexagons.Tests.map
+    ]
+
+
+port runner : Signal (Task.Task x ())
+port runner = 
+    run 
+        <| putStrLn 
+        <| List.foldr (++) "" 
+        <| List.map stringRunner allTests
